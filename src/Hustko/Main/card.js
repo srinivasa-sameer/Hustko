03f7300a46f8251b69ec01670ebbffdf2e548395
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2023-12-06 23:59:27 -0500

    Card updates

--- src/Hustko/Main/card.js
+++ src/Hustko/Main/card.js
@@ -1,15 +1,15 @@
-import './index.css';
+import './card.css';
 import { Link } from 'react-router-dom';
 const Card = (props) => {
   return (
-    <div className="card" style={{ width: '15rem' }}>
+    <div className="card m-1">
       <img className="card-img-top" src={props.image} alt="product image" />
       <div className="card-body">
         <Link className="link" to="">
           <h5 class="card-title">{props.title}</h5>
-          <p class="card-text">{props.description}</p>
-          <p class="card-text">${props.price}</p>
         </Link>
+        <p class="card-text">{props.description}</p>
+        <p class="card-text">${props.price}</p>
       </div>
     </div>
   );
