4929ad52d9dd207d41009ea62e26fbe1de0caee2
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2023-12-06 23:34:12 -0500

    card fix

--- src/Hustko/Main/card.js
+++ src/Hustko/Main/card.js
@@ -1,15 +1,15 @@
-import "./index.css"
-import {Link} from "react-router-dom";
+import './index.css';
+import { Link } from 'react-router-dom';
 const Card = (props) => {
   return (
-    <div className="card m-2" style={{ width: '18rem' }}>
+    <div className="card" style={{ width: '15rem' }}>
       <img className="card-img-top" src={props.image} alt="product image" />
       <div className="card-body">
-          <Link className="link" to="">
-            <h5 class="card-title">{props.title}</h5>
-            <p class="card-text">{props.description}</p>
-            <p class="card-text">${props.price}</p>
-          </Link>
+        <Link className="link" to="">
+          <h5 class="card-title">{props.title}</h5>
+          <p class="card-text">{props.description}</p>
+          <p class="card-text">${props.price}</p>
+        </Link>
       </div>
     </div>
   );
