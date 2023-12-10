91a576214a92a3a6e23f847502e076bf34fdefbd
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2023-12-10 16:50:55 -0500

    card UI update

--- src/Hustko/Main/Card/card.js
+++ src/Hustko/Main/Card/card.js
@@ -1,11 +1,17 @@
 import './card.css';
-import { Link } from 'react-router-dom';
+import { FaRegHeart } from 'react-icons/fa';
+
+const addToFavorite = () => {
+  console.log('Favorite!');
+};
+
 const Card = (props) => {
   return (
-    <div className="card m-2" style={{ width: '18rem' }}>
+    <div className="card m-2">
       <img className="card-img-top" src={props.image} alt="product image" />
       <div className="card-body">
-          <h5 class="card-title">{props.title}</h5>
+        <FaRegHeart className="float-end heartButton" onClick={addToFavorite} />
+        <h5 class="card-title">{props.title}</h5>
         <p class="card-text">{props.description}</p>
         <p class="card-text">${props.price}</p>
       </div>
